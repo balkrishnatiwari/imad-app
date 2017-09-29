21e135b94c4d5c3354449261cbeee9e845f007b9
balkrishnatiwari <31163822+balkrishnatiwari@users.noreply.github.com>
2017-09-29 02:19:38 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.get('/', function (req, res) {
 app.get('article-one',function(req,res){
     res.send('article one resquested and will be served here');
 });
-app.get('article-one',function(req,res){
-    res.send('article one resquested and will be served here');
+app.get('article-two',function(req,res){
+    res.send('article two resquested and will be served here');
 });
-app.get('article-one',function(req,res){
-    res.send('article one resquested and will be served here');
+app.get('article-three',function(req,res){
+    res.send('article three resquested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
